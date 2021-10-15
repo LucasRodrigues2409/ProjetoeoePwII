66027354a96926b32c286d5c1e74c4414cd718d6
LucasRodrigues2409 <92602303+LucasRodrigues2409@users.noreply.github.com>
2021-10-15 16:18:24 -0300

    V2 Insert/Select

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ProjetoPwII
+# CRUD-MVC-PHP
+Crud em MVC e PHP
+### Objetivo: 
+
+>Desenvolver um sistema CRUD simples em PHP 5 ou 7 (sem frameworks) e MySQL para a administração de serviços agendados.
--- model/cadastro.php
+++ model/cadastro.php
@@ -61,9 +61,5 @@ class Cadastro extends Banco {
     public function editar(){
         return $this->updateAgendamentos($this->getId(),$this->getNome(),$this->getTelefone(),$this->getOrigem(),$this->getData_contato(),$this->getObservacao());
     }
-
-    public function excluir($id){
-        return $this->deleteAgendamentos($id);
-    }
 }
 ?>
